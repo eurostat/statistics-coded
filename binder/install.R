eaef0b4701bebf1c1f0e398882156a9da892fe30
Jacopo Grazzini <gjacopo@gmail.com>
2020-02-14 09:30:21 +0100

    Create install.R

---
+++ binder/install.R
@@ -0,0 +1,15 @@
+# install.packages('devtools',repos='https://cloud.r-project.org')
+#install.packages("devtools")
+#devtools::install_github('IRkernel/IRkernel')
+# IRkernel::installspec()
+# devtools::install_github("user/repo", ref = "tag")
+
+install.packages(c("tidyr", "repr", "dplyr"))
+install.packages("ggplot2")
+
+# devtools::install_github("eurostat/restatapi")
+install.packages("restatapi")
+#install.packages(c('eurostat','ggrepel','ggraph','ggiraph','ggnetwork','ggTimeSeries','plotrix','tmap','rsdmx','leaflet','shinyjs'),
+#			repos='https://cloud.r-project.org')
+
+#install.packages("reticulate") # python support in RMarkdown
