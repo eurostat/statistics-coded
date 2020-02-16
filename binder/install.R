72389ecd549b4d51cddd4ec729e54d84ee04eaf1
Jacopo Grazzini <gjacopo@gmail.com>
2020-02-16 22:22:45 +0100

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -2,13 +2,13 @@ install.packages("devtools",repos="https://cloud.r-project.org")
 # devtools::install_github('IRkernel/IRkernel')
 # IRkernel::installspec()
 
-install.packages(c("tidyr", "repr", "dplyr"))
+install.packages(c("tidyr", "repr", "dplyr", "data.table"))
 install.packages("ggplot2")
 
 # Eurostat package restatapi: we get the dev version
 devtools::install_github("eurostat/restatapi")
 # install.packages("restatapi")
-install.packages(c("rsdmx", "RJSDMX"))
+install.packages(c("rsdmx", "RJSDMX","rdbnomics"))
 
 # eurostat packages: many dependencies...
 #install.packages(c('eurostat','ggrepel','ggraph','ggiraph','ggnetwork','ggTimeSeries','plotrix','tmap','leaflet','shinyjs'),
