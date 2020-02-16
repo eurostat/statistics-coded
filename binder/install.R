6296241bbca09bfafd21a32a58ed08f103601130
Jacopo Grazzini <gjacopo@gmail.com>
2020-02-16 23:30:00 +0100

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -1,7 +1,9 @@
 #install.packages("devtools",repos="https://cloud.r-project.org")
-install.packages("devtools", dependencies=TRUE)
+install.packages("devtools")
 # devtools::install_github('IRkernel/IRkernel')
 # IRkernel::installspec()
+
+install.packages("reticulate") # python support in RMarkdown
 
 install.packages(c("tidyr", "repr", "dplyr", "data.table"))
 install.packages("ggplot2")
@@ -9,10 +11,9 @@ install.packages("ggplot2")
 # Eurostat package restatapi: we get the dev version
 devtools::install_github("eurostat/restatapi")
 # install.packages("restatapi")
+
 install.packages(c("rsdmx", "RJSDMX","rdbnomics"))
 
 # eurostat packages: many dependencies...
 #install.packages(c('eurostat','ggrepel','ggraph','ggiraph','ggnetwork','ggTimeSeries','plotrix','tmap','leaflet','shinyjs'),
 #			repos='https://cloud.r-project.org')
-
-install.packages("reticulate") # python support in RMarkdown
