37116e0faaecc411bd3edde35a4769f04bf6cfe0
Jacopo Grazzini <gjacopo@gmail.com>
2020-02-17 13:33:49 +0100

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -1,21 +1,21 @@
-install.packages("devtools", dependencies=TRUE, repos=NULL)
-# install.packages("devtools", dependencies=TRUE, repos="https://cloud.r-project.org")
-# install.packages("devtools", dependencies=TRUE, repos="http://cran.rstudio.com/")
+# install.packages("devtools", dependencies=TRUE, repos=NULL)
+# install.packages("devtools", repos="https://cloud.r-project.org")
+install.packages("devtools", repos="http://cran.rstudio.com/")
 
 # devtools::install_github('IRkernel/IRkernel')
 # IRkernel::installspec()
 
-install.packages(c("tidyr", "repr", "dplyr"), dependencies=TRUE, repos=NULL)
-install.packages(c("ggplot2", "data.table"), repos=NULL)
+install.packages(c("tidyr", "repr", "dplyr"), repos="http://cran.rstudio.com/")
+install.packages(c("ggplot2", "data.table"), repos="http://cran.rstudio.com/")
 
 # Eurostat package restatapi: we get the dev version
 devtools::install_github("eurostat/restatapi")
-install.packages("restatapi", dependencies=TRUE, repos=NULL)
+install.packages("restatapi", repos="http://cran.rstudio.com/")
 
-install.packages(c("rjson", "rsdmx", "RJSDMX","rdbnomics"), repos=NULL)
+install.packages(c("rjson", "rsdmx", "RJSDMX","rdbnomics"), repos="http://cran.rstudio.com/")
 
 # eurostat packages: many dependencies...
 #install.packages(c('eurostat','ggrepel','ggraph','ggiraph','ggnetwork','ggTimeSeries','plotrix','tmap','leaflet','shinyjs'),
 #			repos=NULL)
 
-install.packages("reticulate", repos=NULL) # python support in RMarkdown
+install.packages("reticulate", repos="http://cran.rstudio.com/") # python support in RMarkdown
