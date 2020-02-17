55cd9c78c7dff6b0cb00991e5a466d00f142df6e
Jacopo Grazzini <gjacopo@gmail.com>
2020-02-17 15:57:37 +0100

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -1,24 +1,24 @@
 # install.packages("devtools", dependencies=TRUE, repos=NULL)
-# install.packages("devtools", repos="https://cloud.r-project.org")
-install.packages("devtools", repos="http://cran.rstudio.com/")
+install.packages("devtools", repos="https://cloud.r-project.org")
+# install.packages("devtools", repos="http://cran.rstudio.com/")
 
 # devtools::install_github('IRkernel/IRkernel')
 # IRkernel::installspec()
 
-install.packages(c("tidyr", "repr", "dplyr"), repos="http://cran.rstudio.com/")
-install.packages("ggplot2", repos="http://cran.rstudio.com/")
+install.packages(c("tidyr", "repr", "dplyr"), repos="https://cloud.r-project.org")
+install.packages("ggplot2", repos="https://cloud.r-project.org")
 
-install.packages(c("data.table", "rjson", "xml2"), repos="http://cran.rstudio.com/")
+install.packages(c("data.table", "rjson", "xml2"), repos="https://cloud.r-project.org")
 
 # Eurostat package restatapi: we get the dev version
 devtools::install_github("eurostat/restatapi")
-# install.packages("restatapi", repos="http://cran.rstudio.com/")
+# install.packages("restatapi", repos="https://cloud.r-project.org")
 
-install.packages(c("rsdmx", "RJSDMX", "rdbnomics"), repos="http://cran.rstudio.com/")
-install.packages(c("timeSeries", "ggdemetra", "prophet"), repos="http://cran.rstudio.com/")
+install.packages(c("rsdmx", "RJSDMX", "rdbnomics"), repos="https://cloud.r-project.org")
+install.packages(c("timeSeries", "ggdemetra", "prophet"), repos="https://cloud.r-project.org")
 
 # eurostat packages: many dependencies...
 #install.packages(c('eurostat','ggrepel','ggraph','ggiraph','ggnetwork','ggTimeSeries','plotrix','tmap','leaflet','shinyjs'),
 #			repos=NULL)
 
-install.packages("reticulate", repos="http://cran.rstudio.com/") # python support in RMarkdown
+install.packages("reticulate", repos="https://cloud.r-project.org") # python support in RMarkdown
