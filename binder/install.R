5b9fecaf44a0a5fad6ab0385b5da729220febeb8
Jacopo Grazzini <gjacopo@gmail.com>
2020-02-19 01:39:53 +0100

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -1,18 +1,19 @@
-# install.packages("devtools", dependencies=TRUE, repos=NULL)
-# install.packages("devtools", repos="https://cloud.r-project.org")
-# install.packages("devtools", repos="http://cran.rstudio.com/")
+# if (!requireNamespace("devtools", quietly = TRUE))
+#  install.packages("devtools", repos="https://cloud.r-project.org")
+# #  install.packages("devtools", repos="http://cran.rstudio.com/")
+# #  install.packages("devtools", dependencies=TRUE, repos=NULL)
 
 # devtools::install_github('IRkernel/IRkernel')
 # IRkernel::installspec()
 
-# install through conda conda-forge
-# install.packages(c("tidyr", "repr", "dplyr"), repos="https://cloud.r-project.org")
-# install.packages("ggplot2", repos="https://cloud.r-project.org")
-# install.packages(c("data.table", "rjson", "xml2"), repos="https://cloud.r-project.org")
+# install through conda conda-forge?
+install.packages(c("tidyr", "repr", "dplyr"), repos="https://cloud.r-project.org")
+install.packages("ggplot2", repos="https://cloud.r-project.org")
+install.packages(c("data.table", "rjson", "xml2"), repos="https://cloud.r-project.org")
 # install.packages("reticulate", repos="https://cloud.r-project.org") # python support in RMarkdown
 
 # Eurostat package restatapi: we get the dev version
-install.packages("restatapi", dependencies=TRUE, repos="https://cloud.r-project.org")
+install.packages("restatapi", repos="https://cloud.r-project.org")
 # devtools::install_github("eurostat/restatapi")
 install.packages(c("rsdmx", "rdbnomics"), repos="https://cloud.r-project.org")
 # install.packages(c("timeSeries", "ggdemetra", "prophet"), repos="https://cloud.r-project.org")
