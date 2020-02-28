01f636d0667a9731bec5da83e65d50c9f2aff861
flopaleur <60342444+flopaleur@users.noreply.github.com>
2020-02-28 10:22:44 +0100

    Add files via upload

--- popul/income_consumption_wealth/icw2.R
+++ popul/income_consumption_wealth/icw2.R
@@ -1,8 +1,10 @@
 ############################################################################################################
 # Scripts for computing figures and generating graphs in Statistics Explained article
-# http://ec.europa.eu/eurostat/statistics-explained/index.php?title=Households%27_income,_consumption_and_wealth,_and_how_they_interact_-_statistics_on_main_results
-# Run on R 64-bits 3.3.1
+# https://ec.europa.eu/eurostat/statistics-explained/index.php/Interaction_of_household_income,_consumption_and_wealth_-_methodological_issues
+# Run on R 64-bits 3.3.1 
 
+# Please insert the link to the folder where you want the files to be saved
+path_results <- "./MainResults"
 
 library(dplyr)
 library(haven)
@@ -10,6 +12,7 @@ library(Cairo)
 library(eurostat)
 library(reshape2)
 library(plot3D)
+library(xlsx)
 
 col1 <- rgb(250, 165, 25, maxColorValue = 255)
 col1_faded <- rgb(251, 200, 117, maxColorValue = 255)
@@ -20,6 +23,7 @@ col3_faded <- rgb(246, 162, 123, maxColorValue = 255)
 col4 <- rgb(185, 195, 30, maxColorValue = 255)
 col5 <- rgb(93, 179, 64, maxColorValue = 255)
 
+year <- 2015
 
 #################################################################################################################################################
 ### FIGURE 1
@@ -27,14 +31,17 @@ col5 <- rgb(93, 179, 64, maxColorValue = 255)
 
 joint_dist <- get_eurostat("icw_sr_07", time_format = "num")
 jointDist <- filter(joint_dist, 
-                    geo == "BE")
+                    geo == "BE" & time == year)
 jointDist <- mutate(jointDist,
                     q_inc = as.numeric(substr(quant_inc,2,3)),
                     q_exp = as.numeric(substr(quant_expn,2,3)))
-matJointDist <- acast(data = jointDist, q_exp~q_inc, value.var = "values")/10
+matJointDist <- acast(data = jointDist, q_inc~q_exp, value.var = "values")/1000
 
 hist3D(1:10,1:10, matJointDist, xlab = "Income", ylab = "Consumption", 
        border = "grey", space = 0.2)
+
+setwd(path_results)
+write.xlsx(matJointDist,"DataFiguresSE2.xlsx",sheetName = "Figure 1",append = TRUE)
 
 #################################################################################################################################################
 ### FIGURE 2
@@ -44,9 +51,9 @@ indicators_sr <- get_eurostat("icw_sr_01", time_format = "num")
 indicators_agg <- get_eurostat("icw_sr_08", time_format = "num")
 
 indicators_agg <- filter(indicators_agg,
-                         age == "TOTAL")
+                         age == "TOTAL" & time == year)
 indicators_sr <- filter(indicators_sr,
-                        age == "TOTAL")
+                        age == "TOTAL" & time == year)
 indicators_agg <- mutate(indicators_agg,
                          sr_agg = 100 - values)
 indicators_sr <- mutate(indicators_sr,
@@ -59,6 +66,8 @@ barplot(t(figure2[,2:3]), beside = TRUE, col = c(col1, col2), main = NA,
         names.arg = figure2$geo, cex.names = 0.5, ylim = c(-20,50), cex.axis = 0.7,
         args.legend = list(x = "topleft", bty = "n", border = NA, cex = 0.5))
 
+setwd(path_results)
+write.xlsx(figure2,"DataFiguresSE2.xlsx",sheetName = "Figure 2",append = TRUE)
 
 #################################################################################################################################################
 ### FIGURE 3
@@ -67,22 +76,29 @@ barplot(t(figure2[,2:3]), beside = TRUE, col = c(col1, col2), main = NA,
 indicators_sr <- get_eurostat("icw_sr_08", time_format = "num")
 sr_na <- get_eurostat("nasa_10_ki", time_format = "num")
 sr_agg <- filter(indicators_sr,
-                 age == "TOTAL")
-sr_na_2010 <- filter(sr_na,
-                     time == 2010 & sector == "S14_S15" & na_item == "SRG_S14_S15")
+                 age == "TOTAL" & time == year)
+sr_na_2015 <- filter(sr_na,
+                     time == year & sector == "S14_S15" & na_item == "SRG_S14_S15")
 
-sr_na_2010 <- mutate(sr_na_2010,
+sr_na_2015 <- mutate(sr_na_2015,
                      sr_na = values)
 sr_agg <- mutate(sr_agg,
                  sr_agg = 100 - values)
-sr <- merge(sr_agg, sr_na_2010, by = "geo")
+sr <- merge(sr_agg, sr_na_2015, by = "geo")
 
 plot(sr$sr_agg, sr$sr_na, type = "p", pch = 18, col = col1, bty = "n", xaxt="n",yaxt="n", 
-     xlim = c(0,50), ylim = c(-5,20), xlab = NA, ylab = NA)
+     xlim = c(0,35), ylim = c(-15,25), xlab = NA, ylab = NA)
 grid(nx = NA, ny = NULL)
 axis(1,pos=0)
 axis(2,pos=0, tick = FALSE, las = 1)
 text(sr$sr_agg, sr$sr_na, labels = sr$geo, cex = 0.7, pos = 3)
+
+setwd(path_results)
+write.xlsx(sr,"DataFiguresSE2.xlsx",sheetName = "Figure 3",append = TRUE)
+
+
+
+
 
 
 ### alternative: account for imputed rents
@@ -158,4 +174,3 @@ grid(nx = NA, ny = NULL)
 axis(1,pos=0)
 axis(2,pos=0, tick = FALSE, las = 1)
 text(sr$sr_agg, sr$saving_rate_wo_imprent, labels = sr$geo, cex = 0.7, pos = 3)
-
